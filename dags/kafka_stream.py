c5e4f963c566f16e6c87a424d234ebdb0f20e814
WiljamiT <74732018+WiljamiT@users.noreply.github.com>
2024-03-17 12:09:22 +0200

    kafka

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -38,10 +38,19 @@ def format_data(res):
 
 def stream_data():
     import json
+
+    from kafka import KafkaProducer
+    import time
+
     res = get_data()
     res = format_data(res)
-    print(json.dumps(res, indent=3))
 
+    # print(json.dumps(res, indent=3))
+
+    producer = KafkaProducer(bootstrap_servers=['localhost:9092'], max_block_ms=5000)
+
+    # push data from res to q
+    producer.send('user_created', json.dumps(res).encode('utf-8'))
 
 # with DAG('user_automation',
 #          default_args=default_args,
